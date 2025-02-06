416d85173d11d3930b4144bac985c08026fcf592
Ryan Kuang <96274506+RyanKuang2004@users.noreply.github.com>
2025-02-07 12:08:49 +1300

    Mini update

--- graph.py
+++ graph.py
@@ -1,39 +1,145 @@
 from typing import List, Annotated, Literal
 from typing_extensions import TypedDict
 from langgraph.graph import END, StateGraph, START
+from langchain.schema import Document
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_ollama import ChatOllama
 from langchain_chroma import Chroma
-from prompts import ROUTER_INSTRUCTIONS
+from prompts import ROUTER_INSTRUCTIONS, DOCUMENT_GRADER_INSTRUCTIONS
 from langchain_core.messages import HumanMessage, SystemMessage
 from pydantic import BaseModel, Field
+from langchain_community.tools.tavily_search import TavilySearchResults
+import os
+from dotenv import load_dotenv
+import getpass
+from IPython.display import Image, display
+
+load_dotenv()
+
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = os.getenv(var)
+
+_set_env("OPENAI_API_KEY")
+_set_env("TAVILY_API_KEY")
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 db = Chroma(
     persist_directory='./chroma',
     embedding_function=OpenAIEmbeddings(model="text-embedding-3-small")
 )
+retriever = db.as_retriever(search_kwargs={"k": 4})
+web_search_tool = TavilySearchResults(max_results=4)
 
 llm = ChatOllama(model="deepseek-r1:7b")
 llm_openai = ChatOpenAI(model="gpt-4o-mini")
 
 class GraphState(TypedDict):
-    documents: List[str]
+    documents: List
     question: str
     answer: str
     
 class RouterAnswer(BaseModel):
-    search_type: Literal[str]
-    
+    datasource: Literal["websearch", "vectorstore"] = Field(
+        None, description="Whether the data source should be a web search or vectorstore"
+    )
 
+class DocumentGraderAnswer(BaseModel):
+    binary_score: Literal['yes', 'no'] = Field(
+        ..., description="Indicates whether the document contains relevant information to the question."
+    )
+    
 def route(state: GraphState): 
     router_llm = llm_openai.with_structured_output(RouterAnswer)
     
-    result = llm.invoke(
+    result = router_llm.invoke(
         [SystemMessage(ROUTER_INSTRUCTIONS)]
         + [HumanMessage(state['question'])]
-    ])
-    source = result["search_type"]
+    )
+    source = result["datasource"]
+    
+    if source == "websearch":
+        print("---ROUTING QUESTION TO WEB SEARCH---")
+        return "websearch"
+    elif source == "vectorstore":
+        print("---ROUTING QUESTION TO VECTOR STORE---")
+        return "vectorstore"
+    
+def web_search(state):
+    """
+    Web search based based on the question
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Appended web results to documents
+    """
+
+    print("---WEB SEARCH---")
+
+    # Web search
+    docs = web_search_tool.invoke({"query": state["question"]})
+    web_results = "\n".join([d["content"] for d in docs])
+    web_results = Document(page_content=web_results)
+
+    return {"documents": docs}
+        
+def retrieve(state: GraphState):
+    print("---RETRIEVE---")
+    
+    documents = retriever.invoke(state["question"])
+    return {"documents": documents}
+
+def grade_documents(state: GraphState):
+    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+    question = state["question"]
+    documents = state["documents"]
+    
+    document_grader = llm_openai.with_structured_output(DocumentGraderAnswer)
+    
+    filtered_docs = []
+    for doc in documents:
+        doc_grader_instructions = DOCUMENT_GRADER_INSTRUCTIONS.format(
+            document = doc.page_content,
+            question = question
+        )
+        result = document_grader.invoke(doc_grader_instructions)    
+        
+        if result['binary_score'] == "yes":
+            print("---GRADE: DOCUMENT RELEVANT---")
+            filtered_docs.append(doc)
+        else:
+            # Edit later
+            print("---GRADE: DOCUMENT NOT RELEVANT---")
+            web_search = "Yes"
+            continue
+    
+    return {"documents": filtered_docs}
+
+def generate_answer(state: GraphState):
+    question = state["question"]
+    
+    response_prompt = 
+    answer = llm.invoke(response_prompt)
+    return {"answer": answer}
     
 
-
 workflow = StateGraph(GraphState)
+
+workflow.add_node("websearch", web_search)
+workflow.add_node("retrieve", retrieve)
+workflow.add_node("grade_documents", grade_documents)
+workflow.add_node("generate_answer", generate_answer)
+
+workflow.set_conditional_entry_point(
+    route,
+    {
+        "websearch": "websearch",
+        "vectorstore": "retrieve",
+    },
+)
+workflow.add_edge("retrieve", "grade_documents")
+
+# Add memory
+graph = workflow.compile()
--- prompts.py
+++ prompts.py
@@ -5,3 +5,11 @@ The vectorstore contains documents on machine learning job listings, including r
 Use the vectorstore for queries related to these topics. For all other topics, especially current events, use web search.
 
 Return a JSON object with a single key, 'datasource', set to either 'vectorstore' or 'websearch' based on the query."""
+
+DOCUMENT_GRADER_INSTRUCTIONS = """You are a grader assessing relevance of a retrieved document to a user question.
+
+If the document contains keyword(s) or semantic meaning related to the question, grade it as relevant.
+
+Here is the retrieved document: \n\n {document} \n\n Here is the user question: \n\n {question}. 
+
+This carefully and objectively assess whether the document contains at least some information that is relevant to the question."""
